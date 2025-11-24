2bb6353e2e3cc9546017d975c67f435bdbdf52cd
Omar Reyes L. <relo2210@gmail.com>
2025-11-24 02:25:44 -0600

    feat: implement database connection and data ingestion functions

--- ingest_data.py
+++ ingest_data.py
@@ -1,5 +1,35 @@
 import pandas as pd
 import os
+import psycopg2 as pg
+from sqlalchemy import create_engine
+
+url = '../dataset/uber_reviews_without_reviewid.csv'
+db_name = "proj_data_analysis"
+user = "postgres"
+raw_name = "raw_reviews"
+
+def connect_to_db(db_name, user, password="", host='localhost', port='5432'):
+    """
+    Establish a connection to the PostgreSQL database.
+
+    Parameters:
+    db_name (str): The name of the database.
+    user (str): The database user.
+    password (str): The password for the database user.
+    host (str): The host of the database. Default is 'localhost'.
+    port (str): The port of the database. Default is '5432'.
+
+    Returns:
+    connection: A connection object to the PostgreSQL database.
+    """
+    try:
+        engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}")
+        print("Connection to PostgreSQL established successfully.")
+        return engine
+    except Exception as e:
+        print(f"An error occurred while connecting to the database: {e}")
+        return None
+
 def ingest_data(file_path):
     """
     Ingest data from a CSV file and return a pandas DataFrame.
@@ -21,6 +51,14 @@ def ingest_data(file_path):
     except Exception as e:
         raise RuntimeError(f"An error occurred while reading the CSV file: {e}")
 
-data = ingest_data('../dataset/uber_reviews_without_reviewid.csv')
-df = pd.DataFrame(data)
-print(df.head())  # Display the first few rows of the DataFrame
+
+def raw_to_db(data, engine, sql_table, if_case="replace"):
+    data.to_sql(sql_table, engine, if_exists=if_case, index=False)
+    return
+
+def main():
+    engine = connect_to_db(db_name, user)
+    data = ingest_data(url)
+    raw_to_db(data, engine, raw_name)
+
+main()
