bf2ff65bace0ef5822e31aba6275c3c61ebbdace
Omar Reyes L. <relo2210@gmail.com>
2025-11-27 01:47:17 -0600

    feat: Enhance sentiment analysis with confidence scoring and database integration

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -5,27 +5,27 @@ import nltk
 
 db_name = "proj_data_analysis"
 user = "postgres"
+table_name = "analyzed_reviews"
+
+kw_app = [word.lower() for word in ["app", "helpful", "user friendly", "facilities", "application", "supportive"]]
+kw_finance = [word.lower() for word in ["charged", "pay", "payment", "money", "extra money"]]
+kw_trip = [word.lower() for word in ["service", "trip", "excelent", "safest", "waiting", "driver", "ride", "pickup"]]
 
 def main(db_name, user):
     engine = connect_to_db(db_name, user)
     sql_query = "SELECT review_id, content FROM raw_reviews"
     try:
         df_to_analyze = pd.read_sql(sql_query, engine)
-        print(df_to_analyze)
-        return df_to_analyze
+        return df_to_analyze, engine
     except Exception as e:
         print(f"An error occurred while processing the query: {e}")
 
 def vader(df):
     sia = SentimentIntensityAnalyzer()
-    df["sentimental_rate"] = df["content"].apply(lambda review: sia.polarity_scores(review)['compound'])
+    df["confidence"] = df["content"].apply(lambda review: sia.polarity_scores(review)['compound'])
     return df
 
 def classify_reviews(text):
-    kw_app = [word.lower() for word in ["app", "helpful", "user friendly", "facilities", "application", "supportive"]]
-    kw_finance = [word.lower() for word in ["charged", "pay", "payment", "money", "extra money"]]
-    kw_trip = [word.lower() for word in ["service", "trip", "excelent", "safest", "waiting", "driver", "ride", "pickup"]]
-    
     text = text.lower()    
 
     if set(text.split()) & set(kw_app):
@@ -37,21 +37,19 @@ def classify_reviews(text):
     else:
         return "General"
 
-def classify_sentiment(score):
-    if (score >= 0.9) :
-        return "Very Satisfied"
-    elif (score >= 0.04):
-        return "Satisfied"
-    elif (score <= (-0.9)):
-        return "Very Unsatisfied"
-    elif (score <= (-0.4)):
-        return "Unsatisfied"
-    else:
-        return "Neutral"
+def classify_sentiment(df):
+    bins = [-1.0, -0.9, -0.4, 0.4, 0.9, 1.0]
+    labels = ["Very Unsatisfied", "Unsatisfied", "Neutral", "Satisfied", "Very Satisfied"]
+    df["sentiment"] = pd.cut(df["confidence"], bins=bins, labels=labels)
+    return df
+
+def send_to_db(df, engine, sql_table, if_case="append"):
+    df.to_sql(sql_table, engine, if_exists=if_case, index=False)
+    return
 
 if __name__ == "__main__":
-    df_to_analyze = main(db_name, user)
+    df_to_analyze, engine = main(db_name, user)
     df = vader(df_to_analyze)
-    df["category"] = df["content"].apply(lambda x: classify_reviews(x))
-    df["Sentiment"] = df["sentimental_rate"].apply(lambda x: classify_sentiment(x))
-    print(df)
+    df_to_analyze["category"] = df_to_analyze["content"].apply(lambda x: classify_reviews(x))
+    df = classify_sentiment(df)
+    send_to_db(df[["review_id", "sentiment", "confidence", "category"]], engine, table_name)
