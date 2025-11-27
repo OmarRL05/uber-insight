581614b4cc62eae8b39eb17e6ea261b47ff4f849
Omar Reyes L. <relo2210@gmail.com>
2025-11-27 00:54:39 -0600

    feat: Implement sentiment scoring and category classification logic
    - Added `classify_reviews` function using keyword set intersection for topic detection.
    - Added `classify_sentiment` function to map VADER compound scores to descriptive labels.
    - Integrated classification steps into the main execution flow.

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -1,5 +1,7 @@
-import pandas as pd
 from ingest_data import connect_to_db
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+import nltk
 
 db_name = "proj_data_analysis"
 user = "postgres"
@@ -14,5 +16,42 @@ def main(db_name, user):
     except Exception as e:
         print(f"An error occurred while processing the query: {e}")
 
+def vader(df):
+    sia = SentimentIntensityAnalyzer()
+    df["sentimental_rate"] = df["content"].apply(lambda review: sia.polarity_scores(review)['compound'])
+    return df
+
+def classify_reviews(text):
+    kw_app = [word.lower() for word in ["app", "helpful", "user friendly", "facilities", "application", "supportive"]]
+    kw_finance = [word.lower() for word in ["charged", "pay", "payment", "money", "extra money"]]
+    kw_trip = [word.lower() for word in ["service", "trip", "excelent", "safest", "waiting", "driver", "ride", "pickup"]]
+    
+    text = text.lower()    
+
+    if set(text.split()) & set(kw_app):
+        return "App/Technical"
+    elif set(text.split()) & set(kw_finance):
+        return "Finances"
+    elif set(text.split()) & set(kw_trip):
+        return "Trip"
+    else:
+        return "General"
+
+def classify_sentiment(score):
+    if (score >= 0.9) :
+        return "Very Satisfied"
+    elif (score >= 0.04):
+        return "Satisfied"
+    elif (score <= (-0.9)):
+        return "Very Unsatisfied"
+    elif (score <= (-0.4)):
+        return "Unsatisfied"
+    else:
+        return "Neutral"
+
 if __name__ == "__main__":
-    main(db_name, user)
+    df_to_analyze = main(db_name, user)
+    df = vader(df_to_analyze)
+    df["category"] = df["content"].apply(lambda x: classify_reviews(x))
+    df["Sentiment"] = df["sentimental_rate"].apply(lambda x: classify_sentiment(x))
+    print(df)
